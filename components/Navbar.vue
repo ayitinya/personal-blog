356f2067c791472d81abd07983e8b97afd3a67d8
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2022-11-27 21:10:11 +0000

    style: added hover effect to nav and footer links

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -6,15 +6,15 @@
             <NuxtLink to="https://ayitinya.me" :external="true">
                 <img src="~/assets/images/logo.png" class="h-12 dark:invert" alt="logo" />
             </NuxtLink>
-            <ul class="flex items-center">
+            <ul class="flex items-center text-gray-400">
                 <li class="mx-2">
-                    <NuxtLink to="/">Home</NuxtLink>
+                    <NuxtLink class="hover:text-black dark:hover:text-white transition-all duration-500" to="/">Home</NuxtLink>
                 </li>
                 <li class="px-3">
-                    <NuxtLink to="/articles/">Articles</NuxtLink>
+                    <NuxtLink class="hover:text-black dark:hover:text-white transition-all duration-500" to="/articles/">Articles</NuxtLink>
                 </li>
                 <li class="px-3">
-                    <NuxtLink to="https://ayitinya.me" :external="true">About</NuxtLink>
+                    <NuxtLink class="hover:text-black dark:hover:text-white transition-all duration-500" to="https://ayitinya.me" :external="true">About</NuxtLink>
                 </li>
             </ul>
         </nav>

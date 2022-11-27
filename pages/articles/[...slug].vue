36f4a2689662205b528d85dd1fcf700b93254286
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2022-11-27 16:15:37 +0000

    feat: added test blog

---
+++ pages/articles/[...slug].vue
@@ -0,0 +1,17 @@
+<template>
+    <main class="w-full px-5 py-10 md:px-10 lg:px-24 ">
+        <ContentDoc>
+            <template v-slot="{ doc }">
+                <img v-if="doc.image" :src="doc.image" :alt="doc.alt" class="w-full pb-10" />
+                <div class="prose prose-stone md:prose-lg lg:prose-xl dark:prose-invert">
+                    <span>{{doc.date}}</span>
+                    <h1>{{ doc.title }}</h1>
+                    <ContentRenderer :value="doc" />
+                </div>
+            </template>
+            <template #empty>
+                <h1>Document is empty</h1>
+            </template>
+        </ContentDoc>
+    </main>
+</template>

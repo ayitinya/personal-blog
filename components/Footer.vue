6cab79192d0d6b44fb3afc61502f99c16d91c8f5
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2022-11-27 21:25:10 +0000

    fix: added external=true to fix sitemap not found

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,7 @@
 <template>
     <footer class="flex items-center justify-end text-gray-400">
         <NuxtLink class="hover:text-black dark:hover:text-white transition-all duration-500 mx-4" to="https://github.com/ayitinya" :external="true">Page Source</NuxtLink>
-        <NuxtLink class="hover:text-black dark:hover:text-white transition-all duration-500" to="/sitemap.xml">Sitemap</NuxtLink>
+        <NuxtLink class="hover:text-black dark:hover:text-white transition-all duration-500" to="/sitemap.xml" :external="true">Sitemap</NuxtLink>
     </footer>
 </template>
 

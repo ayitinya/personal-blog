e63413b3c259446e1094213c72575c960f15bd4d
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2022-12-06 20:48:18 +0000

    feat: added cloudinary to nuxt image config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-        '@nuxtjs/tailwindcss',
-        '@nuxt/content',
-        '@nuxtjs/robots',
-        '@nuxt/image-edge',
-    ],
-})
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/content",
+    "@nuxtjs/robots",
+    "@nuxt/image-edge",
+  ],
+  image: {
+    cloudinary: {
+      baseURL: "https://res.cloudinary.com/dnha19v3n/image/upload/",
+    },
+  },
+});

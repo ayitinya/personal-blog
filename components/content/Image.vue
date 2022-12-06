949dcebe1896cadf4275c8de7f5259cfc9caae1d
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2022-12-06 20:48:33 +0000

    feat: created a component for images used in blog

---
+++ components/content/Image.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const props = defineProps<{ src: string, alt: string, caption?: string }>()
+</script>
+
+<template>
+    <figure>
+        <NuxtImg provider="cloudinary" :src="props.src" :alt="props.alt" />
+        <figcaption v-if="props.caption" class="text-center">{{ props.caption }}</figcaption>
+    </figure>
+</template>

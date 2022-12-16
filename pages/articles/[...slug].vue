bd7bdf5c9ad09b02adcf602bb5a87d4635012cec
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2022-12-16 11:22:09 +0000

    chore: linting

--- pages/articles/[...slug].vue
+++ pages/articles/[...slug].vue
@@ -17,26 +17,30 @@ onMounted(() => {
 </script>
 
 <template>
-        <ContentDoc>
-            <template v-slot="{ doc }">
-                <ClientOnly>
-                    <Teleport to="body">
-                        <div id="progress-indicator" ref="progressIndicator" class="h-1 rounded-r fixed top-0 bg-black dark:bg-blue-500"></div>
-                    </Teleport>
-                </ClientOnly>
-                <NuxtImg v-if="doc.providerUrl" provider="cloudinary" format="webp" quality="70" :src="doc.providerUrl" class="w-full pb-10 max-h-[450px] object-cover object-center"/>
-                <NuxtImg v-else-if="doc.image" :src="doc.image" :alt="doc.alt" class="w-full pb-10 max-h-[450px] object-cover object-center" />
-                <article class="mx-auto prose prose-stone md:prose-lg lg:prose-xl dark:prose-invert">
-                    <span>{{ doc.date }}</span>
-                    <h1>{{ doc.title }}</h1>
-                    <ContentRenderer :value="doc" />
-                </article>
-            </template>
-            <template #empty>
-                <h1>Document is empty</h1>
-            </template>
-        </ContentDoc>
+    <ContentDoc>
+        <template v-slot="{ doc }">
+            <ClientOnly>
+                <Teleport to="body">
+                    <div id="progress-indicator" ref="progressIndicator"
+                        class="h-1 rounded-r fixed top-0 bg-black dark:bg-blue-500"></div>
+                </Teleport>
+            </ClientOnly>
+            <NuxtImg v-if="doc.providerUrl" provider="cloudinary" format="webp" quality="70" :src="doc.providerUrl"
+                class="w-full pb-10 max-h-[450px] object-cover object-center" />
+            <NuxtImg v-else-if="doc.image" :src="doc.image" :alt="doc.alt"
+                class="w-full pb-10 max-h-[450px] object-cover object-center" />
+            <article class="mx-auto prose prose-stone md:prose-lg lg:prose-xl dark:prose-invert">
+                <span>{{ doc.date }}</span>
+                <h1>{{ doc.title }}</h1>
+                <ContentRenderer :value="doc" />
+            </article>
+        </template>
+        <template #empty>
+            <h1>Document is empty</h1>
+        </template>
+    </ContentDoc>
 </template>
 
 <style scoped lang="scss">
+
 </style>

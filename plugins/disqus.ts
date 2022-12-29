16ae44616cc0ed0d63fe1bdc5ad0aabdcae2f329
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2022-12-29 02:56:29 +0000

    feat: removes requirement for disqus to work only in production

--- plugins/disqus.ts
+++ plugins/disqus.ts
@@ -1,12 +1,6 @@
 import VueDisqus from "vue-disqus";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const config = useRuntimeConfig();
-
-  if (config.public.ENV !== "production") {
-    return;
-  }
-
   nuxtApp.vueApp.use(VueDisqus, {
     shortname: "ayitinyas-blog",
   });
